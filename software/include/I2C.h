63a5cdbf0fd8398370dc2849f7f45bb180645e3f
Nico Weidenfeller <nico.weidenfeller@gmail.com>
2020-08-04 19:44:23 +0200

    Fixed DocString and small bugs

--- software/include/I2C.h
+++ software/include/I2C.h
@@ -29,9 +29,18 @@ class I2C : public IBaseClass
         bool checkTransmissionError(uint8_t result);
     public:
         // I2C read functions
-        uint8_t read8(uint8_t i2cAddress, uint8_t regAddress);
-        uint16_t read16(uint8_t i2cAddress, uint8_t regAddress);
+        uint8_t read8(uint8_t i2cAddress, 
+                      uint8_t regAddress);
+
+        uint16_t read16(uint8_t i2cAddress, 
+                        uint8_t regAddress);
+
         // I2C write functions
-        bool write8(uint8_t i2cAddres, uint8_t regAddress, uint8_t data);
-        bool write16(uint8_t i2cAddres, uint8_t regAddress, uint16_t data);
+        bool write8(uint8_t i2cAddres, 
+                    uint8_t regAddress, 
+                    uint8_t data);
+
+        bool write16(uint8_t i2cAddres, 
+                     uint8_t regAddress, 
+                     uint16_t data);
 };
--- software/src/I2C.cpp
+++ software/src/I2C.cpp
@@ -41,11 +41,12 @@ void I2C::Run()
  * Reads 8 bit data from a i2c device 8 bit reg
  * 
  * @parameter i2cAddres     The 8 Bit address of the I2C device
- * @parameter regAddress    The 8 Bit address of the register to read from
+ * @parameter regAddress    The 8 Bit address of the 8 Bit register to read from
  * 
  * @return 8 Bit data from the register if successfull, or 0 if not successfull
  **/
-uint8_t I2C::read8(uint8_t i2cAddress, uint8_t regAddress)
+uint8_t I2C::read8(uint8_t i2cAddress, 
+                   uint8_t regAddress)
 {
     // Send register data request to i2c device
     Wire.beginTransmission(i2cAddress);
@@ -79,33 +80,56 @@ uint8_t I2C::read8(uint8_t i2cAddress, uint8_t regAddress)
 /**
  * Reads 16 bit data from a i2c device 16 bit reg
  * 
- * @parameter i2cAddress    I2C-Address of the device, Register Address to read from
+ * @parameter i2cAddres     The 8 Bit address of the I2C device
+ * @parameter regAddress    The 8 Bit address of the 16 Bit register to read from
  * 
- * @return 2 byte data from the reg
+ * @return 16 Bit data from the register if successfull, or 0 if not successfull
  **/
-uint16_t I2C::read16(uint8_t i2cAddress, uint8_t regAddress)
+uint16_t I2C::read16(uint8_t i2cAddress, 
+                     uint8_t regAddress)
 {
     Wire.beginTransmission(i2cAddress);
     Wire.write(regAddress);
-    Wire.endTransmission();
+    uint8_t result = Wire.endTransmission();
+    bool transmissionGood = checkTransmissionError(result);
 
-    Wire.requestFrom((uint8_t)i2cAddress, (uint8_t)2);
-    uint8_t MSB = Wire.read();
-    uint8_t LSB = Wire.read();
+    // Check if transmission was good
+    if(transmissionGood)
+    {
+        // Request data from i2c device
+        Wire.requestFrom((uint8_t)i2cAddress, (uint8_t)2);
+        int MSB = Wire.read();
+        int LSB = Wire.read();
 
-    uint16_t value = ((MSB << 8) | LSB);
-    return value;
+        if(MSB == -1 || LSB == -1)
+        {
+            return 0;
+        }
+        else
+        {
+            uint16_t value = (((uint8_t)MSB << 8) | (uint8_t)LSB);
+            return value;
+        }
+    }
+    else
+    {
+        return 0;
+    }
 };
 
 
 /**
  * Writes 8 bit of data to a i2c device 8 bit register
  * 
- * @parameter i2cAddres     The I2C-Address of the device, Register Address to be written to, 1 byte of data
+ * @parameter i2cAddres     The 8 Bit address of the I2C device
+ * @parameter regAddress    The 8 Bit address of the 8 Bit register to write to
+ * @parameter data          The 8 Bit data to write to the register
  * 
- * @return Successfull write or not
+ * @return True if successfull, false if not
  **/
-bool I2C::write8(uint8_t i2cAddres, uint8_t regAddress, uint8_t data)
+bool I2C::write8(uint8_t i2cAddres, 
+                 uint8_t regAddress, 
+                 uint8_t data)
 {
     Wire.beginTransmission(i2cAddres);
     Wire.write(regAddress);
@@ -117,10 +141,16 @@ bool I2C::write8(uint8_t i2cAddres, uint8_t regAddress, uint8_t data)
 
 /**
  * Writes 16 bit of data to a i2c device 16 bit register
- * @parameter I2C-Address of the device, Register Address to be written to, 2 byte of data
- * @return Successfull write or not
+ * 
+ * @parameter i2cAddres     The 8 Bit address of the I2C device
+ * @parameter regAddress    The 8 Bit address of the 16 Bit register to write to
+ * @parameter data          The 16 Bit data to write to the register
+ * 
+ * @return True if successfull, false if not
  **/
-bool I2C::write16(uint8_t i2cAddres, uint8_t regAddress, uint16_t data)
+bool I2C::write16(uint8_t i2cAddres, 
+                  uint8_t regAddress, 
+                  uint16_t data)
 {
     Wire.beginTransmission(i2cAddres);
     Wire.write(regAddress);
@@ -134,7 +164,9 @@ bool I2C::write16(uint8_t i2cAddres, uint8_t regAddress, uint16_t data)
 
 /**
  * Checks for errors in the result of a Wire.endTransmission()
+ * 
  * @parameter result The result of the Wire.endTransmission()
+ * 
  * @return True if successfull, or false if not
  */ 
 bool I2C::checkTransmissionError(uint8_t result)
