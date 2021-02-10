36f940b806444dd6b2a87ce1c45847cb9b28c0d2
Nico Weidenfeller <nico.weidenfeller@gmail.com>
2021-02-10 19:31:21 +0100

    added missing cli commands for esp and libs

--- ci/build-arduino.sh
+++ ci/build-arduino.sh
@@ -11,13 +11,24 @@ mkdir $HOME/Arduino/libraries
 # Install Arduino IDE
 export PATH=$PATH:$GITHUB_WORKSPACE/bin
 curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | sh
+# Create Config
 arduino-cli config init
+# Add ESP8266 board to config
+arduino-cli config add board_manager.additional_urls https://arduino.esp8266.com/stable/package_esp8266com_index.json
 arduino-cli core update-index
-# Install Arduino AVR core
-arduino-cli core install arduino:avr
+# Install ESP8266 core
+arduino-cli core install esp8266:esp8266
+# List Installed Boards
+arduino-cli core list
+# Install NTPClient Library
+arduino-cli lib install NTPClient
+# Install PubSubClient Library
+arduino-cli lib install pubsubclient
+# Install ArduinoJson Library 
+arduino-cli lib install ArduinoJson
 # Link Arduino library
 ln -s $GITHUB_WORKSPACE $HOME/Arduino/libraries/CI_Test_Library
 # Compile all *.ino files for the Arduino Uno
 for f in **/*.ino ; do
-    arduino-cli compile -b arduino:avr:uno $f
+    arduino-cli compile --fqbn esp8266:esp8266:nodemcu $f
 done
