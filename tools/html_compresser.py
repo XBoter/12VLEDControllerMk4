cad2c11d156a899a41c032940c33e15376b129ab
Nico Weidenfeller <nico.weidenfeller@gmail.com>
2021-12-23 14:59:28 +0100

    Added html compressing and transforming to C ++ Header Python scripts. Also added Github actions for HMTl compression and transforming to C++ Header

---
+++ tools/html_compresser.py
@@ -0,0 +1,59 @@
+import os
+import minify_html
+
+html_uncompressed_folder_local_path = "src\\Webpage\\html_uncompressed"
+html_compressed_folder_local_path = "src\\Webpage\\html_compressed"
+cur_dir = os.getcwd()
+
+# Prints the html page before and after minified
+enable_hml_print = False
+
+html_uncompressed_folder_full_path = os.path.join(os.path.dirname(cur_dir), html_uncompressed_folder_local_path)
+html_compressed_folder_full_path = os.path.join(os.path.dirname(cur_dir), html_compressed_folder_local_path)
+
+print("Uncompressed Folder path '{}'".format(html_uncompressed_folder_full_path))
+print("Compressed Folder path '{}'".format(html_compressed_folder_full_path))
+print("")
+
+html_files = []
+for file in os.listdir(html_uncompressed_folder_full_path):
+    if file.endswith(".html"):
+        html_files.append(file)
+
+print("Found {} uncompressed HTML files".format(len(html_files)))
+print("")
+
+for html_file in html_files:
+
+    html_uncompressed_folder_full_path_with_file = os.path.join(html_uncompressed_folder_full_path,html_file)
+    html_file_content = ""
+    html_file_content_minified = ""
+    with open(html_uncompressed_folder_full_path_with_file) as f:
+        html_file_content = f.read()
+        f.close()
+
+    if html_file_content != "":
+        # minify html page
+        html_file_content_minified = minify_html.minify(html_file_content, minify_js=True,minify_css=True, remove_processing_instructions=True)
+        if enable_hml_print:
+            print('='*10+" ORIGINAL "+'='*10)
+            print(html_file_content)
+            print('='*10+" MINIFIED "+'='*10)
+            print(html_file_content_minified)
+
+        html_compressed_folder_full_path_with_file = os.path.join(html_compressed_folder_full_path,html_file.replace(".html","_compressed.html"))
+
+
+        # save html file to compressed folder
+        with open(html_compressed_folder_full_path_with_file, 'w') as f:
+            f.write(html_file_content_minified)
+            f.close()
+
+        uncompressed_file_size = os.stat(html_uncompressed_folder_full_path_with_file).st_size
+        compressed_file_size = os.stat(html_compressed_folder_full_path_with_file).st_size
+
+        print("Compressed HTML file '{}' reduced size by {:.2f}%".format(html_file, abs((compressed_file_size / uncompressed_file_size) * 100 - 100)))
+
+    else:
+        print("Error reading file '{}'".format(html_uncompressed_folder_full_path_with_file))
+    
---
+++ tools/html_to_c_converter.py
@@ -0,0 +1,45 @@
+import os
+
+html_compressed_folder_local_path = "src\\Webpage\\html_compressed"
+transformed_c_folder_local_path = "src\\Webpage\\transformed_to_c"
+
+cur_dir = os.getcwd()
+
+transformed_c_folder_local_path_full_path = os.path.join(os.path.dirname(cur_dir), transformed_c_folder_local_path)
+html_compressed_folder_full_path = os.path.join(os.path.dirname(cur_dir), html_compressed_folder_local_path)
+
+print("Transformed C File Folder path '{}'".format(transformed_c_folder_local_path_full_path))
+print("Compressed Folder path '{}'".format(html_compressed_folder_full_path))
+print("")
+
+compressed_html_files = []
+for file in os.listdir(html_compressed_folder_full_path):
+    if file.endswith("_compressed.html"):
+        compressed_html_files.append(file)
+
+print("Found {} compressed HTML files".format(len(compressed_html_files)))
+print("")
+
+for compressed_html_file in compressed_html_files:
+
+    html_compressed_folder_full_path_with_file = os.path.join(html_compressed_folder_full_path,compressed_html_file)
+    compressed_html_file_content = ""
+    with open(html_compressed_folder_full_path_with_file) as f:
+        # minify puts all the compressed html code in line one
+        compressed_html_file_content = f.readline()
+        f.close()
+
+    if compressed_html_file_content != "":
+        
+        transformed_c_folder_local_path_full_path_with_file = os.path.join(transformed_c_folder_local_path_full_path,compressed_html_file.replace("_compressed.html",".h"))
+
+        # save html file to compressed fcompressed_html_file_contentolder
+        with open(transformed_c_folder_local_path_full_path_with_file, 'w') as f:
+            data = 'const String {} PROGMEM = "{}";'.format(compressed_html_file.replace("_compressed.html",""),compressed_html_file_content)
+            f.write(data)
+            f.close()
+
+        print("Transformed HTML file '{}' to C Header '{}'".format(compressed_html_file,compressed_html_file.replace("_compressed.html",".h")))
+
+    else:
+        print("Error reading file '{}'".format(html_compressed_folder_full_path_with_file))
